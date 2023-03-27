33bb24655ab7e78ccb8118044744ea71bcabe56c
lyviogd <lyviogd@gmail.com>
2023-03-27 21:36:31 +0200

    add: K8s

---
+++ intallK8s/install_k8s.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+sudo apt-get update && sudo apt-get install -y apt-transport-https ca-certificates curl
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl ipvsadm
+sudo apt-mark hold kubelet kubeadm kubectl
+
+# Ici vous devez être connecté avec votre compte utilisateur et non pas en tant que `root`
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
